5ecd20614985fb964c5435ec71b92d839b5da1be
passionatekoda <oloritunabbey1@gmail.com>
2022-06-22 17:11:03 +0000

    done

--- www/index.php
+++ www/index.php
@@ -1,6 +1,8 @@
 <?php
 define("APP_PATH",dirname(dirname(__FILE__)));
 
+var_dump(APP_PATH);
+
 include APP_PATH."/model/model.php";
 include APP_PATH."/controller/controller.php";
 include APP_PATH."/routes/router.php";
