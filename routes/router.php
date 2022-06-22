bcd5ff6b0bc260828cf8fafc1f34cd0f153ba3c0
passionatekoda <oloritunabbey1@gmail.com>
2022-06-22 15:27:15 +0000

    done

--- routes/router.php
+++ routes/router.php
@@ -3,31 +3,31 @@ $uri=explode("/",$_SERVER['REQUEST_URI']);
 
 switch ($uri[1]) {
   case '':
-    include APP_PATH."/view/login.html";
+    include APP_PATH."/first_site/view/login.html";
     break;
     case 'enter':
-      include APP_PATH."/view/enter.php";
+      include APP_PATH."/first_site/view/enter.php";
       break;
       case 'login':
-        include APP_PATH."/view/login.html";
+        include APP_PATH."/first_site/view/login.html";
         break;
         case 'register':
-          include APP_PATH."/view/register.html";
+          include APP_PATH."/first_site/view/register.html";
           break;
           case 'pass':
-            include APP_PATH."/view/pass.php";
+            include APP_PATH."/first_site/view/pass.php";
             break;
             case 'dashboard':
-              include APP_PATH."/view/dashboard.html";
+              include APP_PATH."/first_site/view/dashboard.html";
               break;
               case 'message':
-                include APP_PATH."/view/message.php";
+                include APP_PATH."/first_site/view/message.php";
                 break;
                 case 'dash':
-                  include APP_PATH."/view/dash.php";
+                  include APP_PATH."/first_site/view/dash.php";
                   break;
                   case 'reload':
-                    include APP_PATH."/view/reload.php";
+                    include APP_PATH."/first_site/view/reload.php";
                     break;
 
 
